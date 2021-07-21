eb3ac02e536d6b8c0edbf2917e3d81170ce7f1f5
Prateek <prateeksdr4@gmail.com>
2021-07-21 09:38:41 +0530

    add some comments in ImageShow for better understanding

--- src/Components/ImageShow/ImageShow.jsx
+++ src/Components/ImageShow/ImageShow.jsx
@@ -4,19 +4,25 @@ import "./ImageShow.css";
 
 function ImageShow() {
 
+  // an array to store all image url
   const imgs = [
     "https://lunchclub.com/static/media/tell.9ad906c2.svg",
     "https://lunchclub.com/static/media/connection.5e02f461.svg",
     "https://lunchclub.com/static/media/conversation.5acf03c4.svg",
   ];
 
+  // using useRef to control image show
   const indexRef = useRef(1);
 
+  // using useState to change state and to set the current state
   const [currImg, setCurrImg] = useState(
     "https://lunchclub.com/static/media/conversation.5acf03c4.svg"
   );
 
   useEffect(() => {
+    
+    // using set timeout method to change images on every 1000ms 
+    // and incrementing indexRef by one to access diff index.  
     const variable = setTimeout(() => {
       setCurrImg(imgs[indexRef.current]);
       indexRef.current = indexRef.current + 1;
@@ -26,6 +32,7 @@ function ImageShow() {
   }, [currImg]);
   // console.log(currImg);
 
+  // showing images by changing background image 
   const imgStyle = {
     backgroundImage: `url(${currImg})`, 
     transition: " all 0.2s ease", 
@@ -36,8 +43,6 @@ function ImageShow() {
       <div className="pt-5">
         <div
           className="showImg"
-          width="250px"
-          height="250px"
           style={imgStyle}
         ></div>
       </div>
